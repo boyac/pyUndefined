9b7e66563f85fb2d6fbc20c4b83b974c8447ef48
Boya Chiou <chinawhitewhy@gmail.com>
2016-08-25 22:15:29 +0900

    Update 9digits_partition.py

--- Project_Quant/9digits_partition.py
+++ Project_Quant/9digits_partition.py
@@ -2,7 +2,7 @@
 # @Author: boyac
 # @Date:   2016-08-25 20:21:18
 # @Last Modified by:   boyac
-# @Last Modified time: 2016-08-25 21:15:17
+# @Last Modified time: 2016-08-25 21:56:47
 
 '''
 Questions:
@@ -16,25 +16,32 @@ import random
 import math 
 
 def simulations(times):
-	p = [0]
+	max_p = []
+	#pp = []
 	for i in xrange(times):
 		num = range(1,10)
 		random.shuffle(num)
 
 		S1 = num[:4]
 		S2 = num[4:]
+		if S1[0] == 9 and S2[0] == 8:
+			# convert list of digits into digits
+			x = int(''.join(map(str,S1))) 
+			y = int(''.join(map(str,S2)))
+			p = x * y
 
-		# convert list of digits into digits
-		x = int(''.join(map(str,S1))) 
-		y = int(''.join(map(str,S2)))
-
-		#while len(p) < 2:
-
-	print len(p) 
+			max_p.append(p)
+	
+	if max(max_p) >= 843973902:
+		print 'Got you', max(max_p)
+	print max_p
 
 
 
 if __name__ == '__main__':
-	print math.factorial(9) * math.factorial(9) # total number of combinations
-	print simulations(10)
-	
+	# answer = 87531*9642 = 843973902
+	print math.factorial(9) * math.factorial(9) # total number of combinations = 131681894400
+	print simulations(10000000)
+	'''
+	Not enough memory to run the simulations.
+	'''
