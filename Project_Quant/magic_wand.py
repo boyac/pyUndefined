bdb4db9f22b5308ff19ab368f50385f2572b3904
Boya Chiou <chinawhitewhy@gmail.com>
2016-09-06 21:04:24 +0900

    optiver_researcher

--- Project_Quant/magic_wand.py
+++ Project_Quant/magic_wand.py
@@ -27,11 +27,12 @@ def magic_wand(num_sig, num_wand):
 	red = []
 	for i in toss:
 		red.append(i.count('R'))
-		# print i
-	# print red
+		#print i
+	#print red
 	# 41 = expected waiting seconds (1/80*(1+80)*80*1/2)
-	print sum([(i-num_wand)*2**(-num_sig)*41 for i in red if i > num_wand]) 
+	return sum([(i-num_wand)*2**(-num_sig)*41 for i in red if i > num_wand]) 
 
 
 if __name__ == '__main__':
-	print magic_wand(2, 1) #10.25, actually I'm not sure the probability part is correct
+	print magic_wand(2, 1) #10.25
+	print magic_wand(3, 1) #25.625
